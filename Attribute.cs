e855cf58f3579e952a3a2505f2769c9a7a0ae25b
dtaylor-530 <d.taylor530@gmail.com>
2020-08-22 16:45:03 +0100

    Fixes AttributeHelper

--- Attribute.cs
+++ Attribute.cs
@@ -9,13 +9,30 @@ namespace UtilityHelper
 {
     public static class AttributeHelper
     {
+        public static string GetDescriptionSafe(object value) => GetAttributeSafe<DescriptionAttribute>(value, a => a.Description);
 
-        public static string GetDescription(Enum value) => value.GetType().GetField(value.ToString()).GetKey<DescriptionAttribute>(a => a.Description);
+        public static string GetAttributeSafe<T>(object value, Func<T, string> func) where T : Attribute => value.GetType().GetAttributeSafe(func);
 
-        public static string GetDescription(this MemberInfo type) => type.GetKey<DescriptionAttribute>(_ => _.Description);
+        public static string GetDescriptionSafe(this MemberInfo type) => type.GetAttributeSafe<DescriptionAttribute>(_ => _.Description);
 
 
-        public static string GetKey<T>(this MemberInfo value, Func<T, string> func) where T : Attribute
+        public static string GetDescription(object value) => GetAttribute<DescriptionAttribute>(value, a => a.Description);
+
+        public static string GetAttribute<T>(object value, Func<T, string> func) where T : Attribute => value.GetType().GetAttribute(func);
+
+        public static string GetDescription(this MemberInfo type) => type.GetAttribute<DescriptionAttribute>(_ => _.Description);
+
+        public static string GetAttribute<T>(this MemberInfo value, Func<T, string> func) where T : Attribute
+        {
+            T[] attributes = (T[])value.GetCustomAttributes(typeof(T), false);
+
+            if (attributes?.Length > 0)
+                return func(attributes[0]);
+            else
+                throw new Exception($"Member has not attributes of type {typeof(T).Name}");
+        }
+
+        public static string GetAttributeSafe<T>(this MemberInfo value, Func<T, string> func) where T : Attribute
         {
             T[] attributes = (T[])value.GetCustomAttributes(typeof(T), false);
 
@@ -25,8 +42,18 @@ namespace UtilityHelper
                 return value.ToString();
         }
 
+        public static string GetAttributeSafe(this MemberInfo value, Func<object, string> func, Type type)
+        {
+            object[] attributes = value.GetCustomAttributes(type, false);
 
-        public static IEnumerable<Type> FilterByCategoryAttribute(this System.Type[] types, string category) => 
+            if (attributes?.Length > 0)
+                return func(attributes[0]);
+            else
+                return value.ToString();
+        }
+
+
+        public static IEnumerable<Type> FilterByCategoryAttribute(this System.Type[] types, string category) =>
             types.Where(type =>
             {
                 var ca = type.GetCustomAttributes(typeof(CategoryAttribute), false).FirstOrDefault();
