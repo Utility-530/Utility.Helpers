2ea92eff6695bb446ca30ad4893764c936740ff1
dtaylor-530 <d.taylor530@gmail.com>
2023-05-28 17:54:49 +0100

    Adds SynchronizationContextHelper

---
+++ Utility.Helpers/SynchronizationContextHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Utility.Helpers
+{
+    /// <summary>
+    /// <a href="https://thomaslevesque.com/2015/11/11/explicitly-switch-to-the-ui-thread-in-an-async-method/"></a>
+    /// </summary>
+    public static class SynchronizationContextHelper
+    {
+        public static SynchronizationContextAwaiter GetAwaiter(this SynchronizationContext context)
+        {
+            return new SynchronizationContextAwaiter(context);
+        }
+    }
+
+    public readonly struct SynchronizationContextAwaiter : INotifyCompletion
+    {
+        private static readonly SendOrPostCallback _postCallback = state => ((Action)state)();
+
+        private readonly SynchronizationContext _context;
+        public SynchronizationContextAwaiter(SynchronizationContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsCompleted => _context == SynchronizationContext.Current;
+
+        public void OnCompleted(Action continuation) => _context.Post(_postCallback, continuation);
+
+        public async void GetResult() {
+        }
+    }
+}
