e95d33ec71129dcf72952118383b11fb833ed916
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-05-29 12:11:20 +0100

    Adds Property2 (PropertyHelper2)

---
+++ Utility.Helpers/Property2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Helpers
+{
+    /// <summary>
+    /// <a href="https://stackoverflow.com/questions/5031288/i-need-to-access-a-non-public-member-highlighted-item-of-a-combo-box"/>
+    /// </summary>
+    public static class PropertyHelper2
+    {
+        /// <summary>
+        /// Returns a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <returns>PropertyValue</returns>
+        public static T GetPrivatePropertyValue<T>(this object obj, string propName)
+        {
+            return (T)obj.GetPrivatePropertyValue(propName);
+        }
+
+        /// <summary>
+        /// Returns a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <returns>PropertyValue</returns>
+        public static object GetPrivatePropertyValue(this object obj, string propName)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            PropertyInfo pi = obj.GetType().GetProperty(propName,
+                                                        BindingFlags.Public | BindingFlags.NonPublic |
+                                                        BindingFlags.Instance) ?? throw new ArgumentOutOfRangeException("propName",
+                                                      string.Format("Property {0} was not found in Type {1}", propName,
+                                                                    obj.GetType().FullName));
+            return pi.GetValue(obj, null);
+        }
+
+        /// <summary>
+        /// Returns a private Field Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Field</typeparam>
+        /// <param name="obj">Object from where the Field Value is returned</param>
+        /// <param name="propName">Field Name as string.</param>
+        /// <returns>FieldValue</returns>
+        public static T GetPrivateFieldValue<T>(this object obj, string propName)
+        {
+            return (T)obj.GetPrivateFieldValue(propName);
+        }
+
+        /// <summary>
+        /// Returns a private Field Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Field</typeparam>
+        /// <param name="obj">Object from where the Field Value is returned</param>
+        /// <param name="propName">Field Name as string.</param>
+        /// <returns>FieldValue</returns>
+        public static object? GetPrivateFieldValue(this object obj, string propName)
+        {
+            if (obj.TryGetPrivateFieldValue(propName, out var value))
+                return value;
+            else
+                throw new ArgumentOutOfRangeException("propName",
+                                               string.Format("Field {0} was not found in Type {1}", propName,
+                                                             obj.GetType().FullName));
+        }
+
+
+        public static bool TryGetPrivateFieldValue(this object obj, string propName, out object? output)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            Type t = obj.GetType();
+            FieldInfo fi = null;
+            while (fi == null && t != null)
+            {
+                fi = t.GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                t = t.BaseType;
+            }
+            if (fi == null)
+            {
+                output = default;
+                return false;
+            }
+            output = fi.GetValue(obj);
+            return true;
+        }
+
+        /// <summary>
+        /// Sets a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is set</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <param name="val">Value to set.</param>
+        /// <returns>PropertyValue</returns>
+        public static void SetPrivatePropertyValue<T>(this object obj, string propName, T val)
+        {
+            Type t = obj.GetType();
+            if (t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
+                throw new ArgumentOutOfRangeException("propName",
+                                                      string.Format("Property {0} was not found in Type {1}", propName,
+                                                                    obj.GetType().FullName));
+            t.InvokeMember(propName,
+                           BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty |
+                           BindingFlags.Instance, null, obj, new object[] { val });
+        }
+
+
+        /// <summary>
+        /// Set a private Field Value on a given Object. Uses Reflection.
+        /// </summary>
+        /// <typeparam name="T">Type of the Field</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Field name as string.</param>
+        /// <param name="val">the value to set</param>
+        /// <exception cref="ArgumentOutOfRangeException">if the Property is not found</exception>
+        public static void SetPrivateFieldValue<T>(this object obj, string propName, T val)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            Type t = obj.GetType();
+            FieldInfo fi = null;
+            while (fi == null && t != null)
+            {
+                fi = t.GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                t = t.BaseType;
+            }
+            if (fi == null)
+                throw new ArgumentOutOfRangeException("propName",
+                                                      string.Format("Field {0} was not found in Type {1}", propName,
+                                                                    obj.GetType().FullName));
+            fi.SetValue(obj, val);
+        }
+    }
+
+}
