ee66c32f0e556a417a41a07762f89b7ee3453aa8
dtaylor-530 <d.taylor530@gmail.com>
2024-08-13 16:19:15 +0100

    Utility.Extensions Increases the options for using TreeExtensions' ExploreTree method

--- Utility.Extensions/TreeExtensions.cs
+++ Utility.Extensions/TreeExtensions.cs
@@ -45,6 +45,11 @@
             return ExploreTree(items, funcAdd, funcRemove, funcClear, property, a => a.Items.Changes<TR>(), predicate ??= (TR a) => true);
         }
 
+        public static IDisposable ExploreTree<T, TR, TS>(TS item, Func<TS, T> funcItems, Func<T, TR, TS, TS> funcAdd, Action<T, TR> funcRemove, Action<T> funcClear, TR property, Predicate<TR>? predicate = default) where TR : IItems
+        {
+            return ExploreTree(item, funcItems, funcAdd, funcRemove, funcClear, property, a => a.Items.Changes<TR>(), predicate ??= (TR a) => true);
+        }
+
         public static IDisposable ExploreTree<T, TR>(T items, Func<T, TR, T> funcAdd, Action<T, TR> funcRemove, Action<T> funcClear, TR property, Func<TR, IObservable<Changes.Set<TR>>> func, Predicate<TR>? funcPredicate = null)
         {
             if (funcPredicate?.Invoke(property) == false)
@@ -63,6 +68,37 @@
                             funcRemove(items, _value);
                         else if (item is Change { Type: Type.Reset })
                             funcClear(items);
+                    }
+                },
+                e =>
+                {
+                },
+                () =>
+                {
+                }
+              );
+            return disposable;
+        }
+
+        public static IDisposable ExploreTree<T, TR, TS>(TS item, Func<TS, T> funcItems, Func<T, TR, TS, TS> funcAdd, Action<T, TR> funcRemove, Action<T> funcClear, TR property, Func<TR, IObservable<Changes.Set<TR>>> func, Predicate<TR>? funcPredicate = null)
+        {
+            if (funcPredicate?.Invoke(property) == false)
+                return Disposable.Empty;
+
+            var items = funcItems(item);
+            item = funcAdd(items, property, item);
+
+            var disposable = func(property)
+                .Subscribe(args =>
+                {
+                    foreach (var _item in args)
+                    {
+                        if (_item is Change { Type: Type.Add, Value: TR value })
+                            _ = ExploreTree(item, funcItems, funcAdd, funcRemove, funcClear, value, func, funcPredicate);
+                        else if (_item is Change { Type: Type.Remove, Value: TR _value })
+                            funcRemove(funcItems(item), _value);
+                        else if (_item is Change { Type: Type.Reset })
+                            funcClear(funcItems(item));
                     }
                 },
                 e =>
