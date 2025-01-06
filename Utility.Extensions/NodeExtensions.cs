fe03d4a9e1a041cabbd54725c62d1ab73c736aef
dtaylor-530 <d.taylor530@gmail.com>
2025-01-06 17:15:36 +0000

    Utility.Extensions Adds NodeExtensions

---
+++ Utility.Extensions/NodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Utility.Trees.Abstractions;
+using Utility.Nodes;
+
+namespace Utility.Extensions
+{
+    public static class NodeExtensions
+    {
+        public static ITree ToTree(Assembly[] assemblies, Predicate<Type>? typePredicate = null)
+        {
+            ViewModelTree t_tree = new("root", "root");
+
+            foreach (var assembly in assemblies)
+            {
+                ViewModelTree tree = new(assembly.GetName().Name, assembly);
+
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (typePredicate?.Invoke(type) == false)
+                        continue;
+                    var _tree = new ViewModelTree(type.Name, type)
+                    {
+                        Parent = tree
+                    };
+                    tree.Add(_tree);
+                }
+                t_tree.Add(tree);
+            }
+            return t_tree;
+        }    
+    }
+}
