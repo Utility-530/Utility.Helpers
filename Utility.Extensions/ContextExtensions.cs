c6574149898c7e5b95d398e5e7c69366b7c98e18
dtaylor-530 <d.taylor530@gmail.com>
2025-01-29 16:44:51 +0000

    Utility.Extensions Adds ContextExtensions Utility.Nodes.Filters Uses new method UI from ContextExtensions

---
+++ Utility.Extensions/ContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Extensions
+{
+    public static class ContextExtensions
+    {
+        public static void UI(this IContext context, Action action)
+        {
+            if (SynchronizationContext.Current != null)
+            {
+                action();
+            }
+            else
+            {
+                context.UI(action);
+            }
+
+        }
+    }
+}
