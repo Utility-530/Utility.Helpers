0b747af628bb2f8ae3919ef478bf133f858778d1
dtaylor-530 <d.taylor530@gmail.com>
2025-08-25 12:32:24 +0100

    Utility.Extensions Adds InterfaceExtensions

---
+++ Utility.Extensions/InterfaceExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Extensions
+{
+    public static class InterfaceExtensions
+    {
+        public static string Key(this IGetKey getKey) => getKey.Key;
+        public static void SetKey(this ISetKey getKey, string key) => getKey.Key = key;
+    }
+}
