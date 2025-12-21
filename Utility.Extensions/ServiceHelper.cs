fa0114fbb50ff6dbef6d81f0c61951f44abff2ad
dtaylor-530 <d.taylor530@gmail.com>
2025-12-21 09:39:35 +0000

    Utility.Extensions Improves logic of ServiceHelper's Observe and ReactTo methods

--- Utility.Extensions/ServiceHelper.cs
+++ Utility.Extensions/ServiceHelper.cs
@@ -1,15 +1,63 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Reactive.Linq;
+using LanguageExt.ClassInstances;
 using Utility.Interfaces.Exs;
 using Utility.Interfaces.Exs.Diagrams;
 using Utility.Interfaces.Methods;
 using Utility.Interfaces.NonGeneric;
+using Utility.Models.Trees;
+using Utility.Nodes;
 using Utility.Observables.Generic;
 using Utility.PropertyNotifications;
 using Utility.ServiceLocation;
+using Utility.Interfaces;
 
 namespace Utility.Extensions
 {
+    public class PropertyNode : IPropertyNode
+    {
+        public PropertyNode()
+        {
+        }
+
+        public ICollection<object> Outputs { get; } = new List<object>();
+        public ICollection<object> Inputs { get; } = new List<object>();
+    }
+
+    public interface IPropertyNode
+    {
+        ICollection<object> Outputs { get; }
+        ICollection<object> Inputs { get; }
+    }
+
+    public class PropertyReceiver<TInput> : IObserver<object>
+    {
+        private readonly INodeViewModel nodeViewModel;
+
+        public PropertyReceiver(INodeViewModel nodeViewModel)
+        {
+            this.nodeViewModel = nodeViewModel;
+        }
+
+        public required string Name { get; set; }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(object value)
+        {
+            (nodeViewModel as ISetValue).Value = value;
+            nodeViewModel.RaisePropertyChanged(nameof(IGetValue.Value));
+        }
+    }
+
     public static class ServiceHelper
     {
         public static void Observe<TParam>(this IServiceResolver serviceResolver, INodeViewModel tModel, bool includeInitial = true) where TParam : IParameter
@@ -18,43 +66,55 @@ namespace Utility.Extensions
             {
                 throw new Exception("f 333333");
             }
-            serviceResolver.Observe<TParam>(tModel.WhenReceivedFrom(a => (a as IGetValue).Value, includeInitialValue: includeInitial, includeNulls: false));
+            var propertyObservable = tModel.WhenReceivedFrom(a => (a as IGetValue).Value, includeInitialValue: includeInitial, includeNulls: false);
+            IPropertyNode propertyNode;
+            if (tModel.Data() is not IPropertyNode node)
+            {
+                propertyNode = new PropertyNode() { };
+                tModel.SetData(propertyNode);
+            }
+            else
+            {
+                propertyNode = node;
+            }
+            if(propertyNode.Inputs.Contains(propertyObservable, new ObserveEqualityComparer()))
+            {
+                return;
+            }
+            propertyNode.Inputs.Add(propertyObservable);
+            serviceResolver.Observe<TParam>(propertyObservable);
         }
 
         public static void ReactTo<TParam, TInput, TOutput>(this IServiceResolver serviceResolver, INodeViewModel tModel, Func<TInput, TOutput>? transformation = null) where TParam : IParameter
         {
-            ReactTo<TParam, TInput>(serviceResolver, new Action<TInput>(a =>
+       
+            IPropertyNode propertyNode;
+            if (tModel.Data() is not IPropertyNode node)
             {
-                TOutput? output = default;
-                if (transformation != null)
-                    output = transformation.Invoke(a);
-                else if (a is TOutput x)
-                    output = x;
-                else if (typeof(TOutput).IsAssignableFrom(typeof(TInput)))
-                {
-                    output = (TOutput)(object)a;
-                }
-                else if (a is IConvertible && typeof(TOutput).IsPrimitive)
-                {
-                    output = (TOutput)Convert.ChangeType(a, typeof(TOutput));
-                }
-                else
-                {
-                    throw new InvalidCastException($"Cannot convert from {typeof(TInput)} to {typeof(TOutput)}");
-                }
+                propertyNode = new PropertyNode();
+                tModel.SetData(propertyNode);
+            }
+            else
+            {
+                propertyNode = node;
+            }
 
-                (tModel as ISetValue).Value = output;
-                tModel.RaisePropertyChanged(nameof(IGetValue.Value));
-            }), tModel);
+            var propertyReceiver = new PropertyReceiver<TOutput>(tModel) { Name = nameof(ISetValue.Value) };
+            if (propertyNode.Inputs.Contains(propertyReceiver, new ObservableEqualityComparer()))
+            {
+                return;
+            }
+            propertyNode.Outputs.Add(propertyReceiver);
+            serviceResolver.ReactTo<TParam, TInput>(propertyReceiver);
         }
 
         public static void ReactTo<TParam, TInput>(this IServiceResolver serviceResolver, Action<TInput> setAction, object? reference = null) where TParam : IParameter
         {
-            var observer = new Reactives.Observer<TInput>(a =>
+            var observer = new Reactives.Observer<object>(a =>
             {
                 Globals.UI.Send((_) =>
                 {
-                    setAction(a);
+                    setAction((TInput)a);
                 }, null);
             }, e => { throw e; }, () => { })
             { Reference = reference };
@@ -95,6 +155,32 @@ namespace Utility.Extensions
             public new bool Equals(object? x, object? y) => x == y;
 
             public int GetHashCode([DisallowNull] object obj) => obj.GetHashCode();
+        }
+    }
+
+    internal class ObservableEqualityComparer : IEqualityComparer<object>
+    {
+        public new bool Equals(object? x, object? y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode([DisallowNull] object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ObserveEqualityComparer : IEqualityComparer<object>
+    {
+        public new bool Equals(object? x, object? y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode([DisallowNull] object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
